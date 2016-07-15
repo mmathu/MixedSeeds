319f501fb9dd14783253f4b24b618703149d6b70
Myoran Mathusuthan <mmathusuthan@gmail.com>
2016-07-15 19:35:14 +0100

    Update dbconnection.php

--- php/dbconnection.php
+++ php/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'KedQ5FZRdBCnzSBD', 'mixedseeds');
+$conn = mysqli_connect(INFORMATION REMOVED FOR OBVIOUS REASONS);
 if(!$conn) {
     die('Database Connection Failed' . mysqli_connect_error());
 }

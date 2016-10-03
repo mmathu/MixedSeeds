6f955886852eb4be6cbecfd791d2efe43f2e8ade
Myoran Mathusuthan <mmathusuthan@gmail.com>
2016-10-03 11:45:22 +0100

    Update dbconnection.php

--- php/dbconnection.php
+++ php/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect(INFORMATION REMOVED FOR OBVIOUS REASONS);
+$conn = mysqli_connect(INSERT DB CONNECTION INFORMATION);
 if(!$conn) {
     die('Database Connection Failed' . mysqli_connect_error());
 }
